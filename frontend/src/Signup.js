e496fe9842e5abdcad44dba9545a42e9f7485dce
“Elie <elie.maj@hotmail.com>
2022-06-25 21:05:02 +0300

    feat (signup) add signup functionality

--- frontend/src/Signup.js
+++ frontend/src/Signup.js
@@ -1,3 +1,44 @@
 import '.styles/styles.css';
+import { useState } from 'react';
+
+function Signup() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signupUser = async () =>{   
+        try {
+            const res = await fetch("http://127.0.0.1:3001/signup", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*'
+                },
+                body: JSON.stringify({
+                    username,
+                    password
+                })
+            });
+            const data = await res.json();
+            debugger
+            if (data.status === 'success') {
+                localStorage.setItem('token', data.data);
+            }
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
+    return(
+        <div className="signup">
+            <div className="signup-form">
+                <h1>Signup</h1>
+                <input type="text" placeholder="Username" onBlur={(e)=>{ setUsername(e.currentTarget.value) }} />
+                <input type="password" placeholder="Password" onBlur={(e)=>{ setPassword(e.currentTarget.value) }}/>
+                <button onClick={signupUser} >Signup</button>
+            </div>
+        </div>
+    )
+
+}
 
 export default Signup;
